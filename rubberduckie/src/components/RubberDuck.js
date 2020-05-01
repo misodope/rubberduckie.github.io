8b4325afe77c5cc75fa9764b9fddaf6857885a58
Jerry Chen <misodope@cap-rx.com>
2020-05-01 13:10:58 -0400

    Adding threejs

--- rubberduckie/src/components/RubberDuck.js
+++ rubberduckie/src/components/RubberDuck.js
@@ -1,19 +1,55 @@
-import React from 'react';
-import "./RubberDuck.scss";
-import "./BuyMeACoffe";
-import BuyMeACoffe from './BuyMeACoffe';
+import React, { useEffect, useRef } from "react"
+import "./RubberDuck.scss"
+import "./BuyMeACoffe"
+import BuyMeACoffe from "./BuyMeACoffe"
+import * as THREE from "three"
 
 const RubberDuck = () => {
-    return (
-        <div className="rubber-duckie">
-            <div className="rubber-duckie__container">
-                <div>Rubber Duckie</div>
-            </div>
-            <div className="rubber-duckie__cta">
-                <BuyMeACoffe/>
-            </div>
-        </div>
-    );
-};
+  const ref = useRef();
 
-export default RubberDuck;
+  useEffect(() => {
+    var scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+
+    var renderer = new THREE.WebGLRenderer({ alpha: true });
+    renderer.setClearColor( 0xffffff, 0 );
+    renderer.setSize(500, 500);
+
+    ref.current.appendChild(renderer.domElement);
+ 
+    var geometry = new THREE.SphereGeometry( 2, 32, 32);
+    var material = new THREE.MeshStandardMaterial();
+    material.color.set(0xfeae09);
+    var sphere = new THREE.Line( geometry, material );
+    scene.add( sphere );
+    camera.position.z = 5;
+
+    function animate() {
+      requestAnimationFrame( animate );
+      sphere.rotation.x += 0.01;
+      sphere.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    }
+    animate();
+  }, [])
+
+  return (
+    <div className="rubber-duckie">
+      <div className="rubber-duckie__container">
+        <div className="rubber-duckie__container--text">Rubber Duckie</div>
+        <div ref={ref} />
+      </div>
+      <div className="rubber-duckie__cta">
+        <BuyMeACoffe />
+      </div>
+    </div>
+  )
+}
+
+export default RubberDuck
