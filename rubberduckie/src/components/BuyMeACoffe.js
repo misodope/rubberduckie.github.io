4bfe5c98a1ab6dca0b1d8a5eb9df9b583d194c49
Jerry Chen <misodope@cap-rx.com>
2020-05-01 00:35:55 -0400

    New Files

---
+++ rubberduckie/src/components/BuyMeACoffe.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BuyMeACoffee.scss";
+
+export default () => {
+    return (
+        <a href="https://www.buymeacoffee.com/misodope" target="_blank">
+            <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" />
+        </a>
+    )
+}
--- rubberduckie/src/components/RubberDuck.js
+++ rubberduckie/src/components/RubberDuck.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./RubberDuck.scss";
+import "./BuyMeACoffe";
+import BuyMeACoffe from './BuyMeACoffe';
 
 const RubberDuck = () => {
     return (
         <div className="rubber-duckie">
-            <div className="rubber-duckie__container">Rubber Duckie</div>
+            <div className="rubber-duckie__container">
+                <div>Rubber Duckie</div>
+            </div>
+            <div className="rubber-duckie__cta">
+                <BuyMeACoffe/>
+            </div>
         </div>
     );
 };
